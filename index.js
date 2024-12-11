7ba73b60cffd6070a0f1f3fe01632dc1ced6d58b
ytrav <withertrav394@gmail.com>
2024-12-11 21:07:30 +0100

    reverted origin post-fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://bank.maevetopia.fun",
   })
 );
 app.use(express.json());
